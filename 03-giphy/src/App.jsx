1b9e235e9560bba5fb2fd290e6f8baa75d83d179
Oliiwerr <99157588+Oliiwerr@users.noreply.github.com>
2023-06-28 09:56:16 -0600

    piquito de oro

--- 03-giphy/src/App.jsx
+++ 03-giphy/src/App.jsx
@@ -1,8 +1,19 @@
-
+import {useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
-
+const[gifs, setGifs] = useState([])
+const API_KEY = 'hN8CgmKNrDPLom6owujZJdr8hwcR5m72'
+useEffect(() => {
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=${AP_IKEY}&limit=25&offset=0&rating=g&lang=es&bundle=messaging_non_clips`)
+}, [])
+.then((response) => {
+  return response.json()
+}) .then((results) => {
+  setGifs (results.data)
+}) .catch((error) =>{
+  console.error(error)
+})
   return (
     <>
       <h1>Giphy App</h1>
